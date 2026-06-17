0aed332060f7bd8a842857c40d172effd426c81f
Muntadas Liam <liam.muntadas@epfedu.fr>
2026-06-17 15:56:42 +0200

    commit de L 17/06 a 15h56

--- src/nettoyage.py
+++ src/nettoyage.py
@@ -7,7 +7,9 @@ def charger_intersections(path, ville):
     #on recupere les lignes correspondantes à la ville saisie
     colonne= df.columns[0]
     df_ville= df[df[colonne].str.contains(ville, case=False)]
-    
+    print(df.head())
+    print("Nouveau")
+    print(df_ville.head())
     #on remet au propre le tableau avec des colonnes disctinctes
     lignes=[next(csv.reader([ligne])) for ligne in df_ville[colonne]]
     tableauFinal = pd.DataFrame(lignes,columns=["type","geometry/type","Longitude",
@@ -18,11 +20,12 @@ def charger_intersections(path, ville):
             "Code Département"
         ]
     )
+    print(tableauFinal["Ville/Commune"].head())
     #on distinct les colonnes Ville et Département à partir de la colonne Ville/Commune
-    tableauFinal[["Ville", "Département"]] = (
-        tableauFinal["Ville/Commune"]
-        .str.split(",", expand=True)
-    )
+    tableauFinal[["Ville", "Département"]] = (tableauFinal["Ville/Commune"]
+        .str.split(",", n=1, expand=True))
+    
+    
     #on corrige les fautes dù au texte encodé
     tableauFinal = correction_intersections(tableauFinal)
     tableauFinal = normailisation_intersections(tableauFinal)
@@ -54,23 +57,170 @@ def correction_intersections(tableauFinal):
 
 def normailisation_intersections(tableauFinal):
     # Normaliser les noms de colonnes
-    #df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+    col="Intersections"
+    df=tableauFinal
+    
+    remplacer = [
+        # Déterminants
+        ("de", "de "),
+        ("de s", "des "),
+        ("du", " du "),
+        ("la", "la "),
+        ("D", " D"),
+
+        # Types de voies
+        ("Rue", "Rue "),
+        ("/", " / "),
+        ("Avenue", "Avenue "),
+        ("Boulevard", "Boulevard "),
+        ("Rond-Point", "Rond-Point "),
+        ("Allée", "Allée "),
+        ("Passage", "Passage "),
+        ("Voie", "Voie "),
+        ("Réside nce", "Résidence "),
+        ("Pla ce", "Place "),
+        ("Square", "Square "),
+
+        # Prénoms
+        ("Victor", "Victor "),
+        ("Amaury", "Amaury "),
+        ("Abbé", "Abbé "),
+        ("Jean", "Jean "),
+        ("Benoît", "Benoît "),
+        ("Gabriel", "Gabriel "),
+        ("Albert", "Albert "),
+        ("Maurice", "Maurice "),
+        ("Charles", "Charles "),
+        ("Georges", "Georges "),
+        ("Baptiste", "Baptiste "),
+        ("Henri", "Henri "),
+        ("Léon", "Léon "),
+        ("Louis", "Louis "),
+        ("Léo", "Léo "),
+        ("Romain", "Romain "),
+        ("Pierre", "Pierre "),
+        ("Paul", "Paul "),
+        ("Jules", "Jules "),
+        ("Alphonse", "Alphonse "),
+        ("René", "René "),
+        ("Aristi de", "Aristide "),
+        ("Rolla nd", "Rolland"),
+        ("Made leine", "Madeleine"),
+        ("Ambroise", "Ambroise "),
+        ("François", "François "),
+        ("Gustave", "Gustave "),
+        ("Hippolyte", "Hippolyte "),
+        ("Marc", "Marc "),
+        ("Marc el", "Marcel "),
+        ("Rene", "Rene "),
+        ("Laurent", "Laurent "),
+        ("Abraham", "Abraham "),
+        ("Salvador", "Salvador "),
+        ("Etienne", "Etienne "),
+        ("Clément", "Clément "),
+        ("Félix", "Félix "),
+        ("Estienne", "Estienne "),
+        ("Esther", "Esther "),
+        ("Léo n-Maurice", "Léon-Maurice "),
+        ("Léo n ce", "Léonce "),
+        ("Cla ude", "Claude "),
+        ("Athime", "Athime "),
+        ("Raymond", "Raymond "),
+        ("André", "André "),
+        ("Guilla ume", "Guillaume "),
+
+        # Noms propres
+        ("Holla nde", "Hollande"),
+        ("Rabela is", "Rabelais"),
+        ("Harde nberg", "Hardenberg "),
+        ("Bla nchard", "Blanchard "),
+        ("Garla nde", "Garlande "),
+        ("Bas", "Bas "),
+        ("Bla ins", "Blains "),
+        ("Haj du", "Hajdu "),
+        ("ÉtienneHajdu", "Étienne Hajdu "),
+        ("ErnestRenan", "Ernest Renan "),
+        ("Dumontd'Urville", "Dumont d'Urville "),
+        ("AugusteBuisson", "Auguste Buisson "),
+        ("SouvenirFrançais", "Souvenir Français "),
+        ("ÉtienneMarcel", "Étienne Marcel "),
+
+        # Noms communs
+        ("Vailla nt", "Vaillant"),
+        ("11", "11 "),
+        ("Treize", "Treize "),
+        ("Ormes", "Ormes "),
+        ("Pla nes", "Planes "),
+        ("Marin", "Marin "),
+        ("Droits", "Droits "),
+        ("Pont", "Pont "),
+        ("Clos", "Clos "),
+        ("Tram", "Tram "),
+        ("Pla te", "Plate "),
+        ("Maréchal", "Maréchal "),
+        ("Docteur", "Docteur "),
+        ("Colonel", "Colonel "),
+        ("Général", "Général "),
+        ("Division", "Division "),
+        ("Parking", "Parking "),
+        ("Espla nade", "Esplanade "),
+        ("Pla isance", "Plaisance "),
+        ("Pla ine", "Plaine "),
+        ("Marquis", "Marquis "),
+        ("Porte", "Porte "),
+        ("Côte", "Côte "),
+        ("Cours", "Cours "),
+
+        # Arbres
+        ("Bouleaux", "Bouleaux "),
+        ("Érables", "Érables "),
+        ("Erables", "Érables "),
+        ("Féviers", "Féviers "),
+        ("Frênes", "Frênes "),
+        ("Marronniers", "Marronniers "),
+        ("Noisetiers", "Noisetiers "),
+        ("Prunier", "Prunier "),
+        ("Ver du n", "Verdun"),
+        ("Martyrs", "Martyrs "),
+        ("Noyers", "Noyers "),
+        ("Tulipiers", "Tulipiers "),
+        ("Tilleuls", "Tilleuls "),
+        ("Marcel in", "Marcelin "),
+        ("Lilas", "Lilas "),
+        ("Sente", "Sente "),
+        ("Hautes", "Hautes "),
+        ("Professeur", "Professeur "),
+        ("Jardin", "Jardin "),
+
+        # Spécificités locales
+        ("Bourg-la -Reine", "Bourg-la-Reine"),
+        ("LaCouléeVerte", "La Coulée Verte"),
+        ("19Janvier", "19 janvier"),
+
+        (" -", "-")
+    ]
+
+    for ancien, nouveau in remplacer:
+        df[col] = df[col].str.replace(ancien,nouveau,regex=False)
 
     return df
 
 def doublons_intersections(tableauFinal):
-    # Identifier les doublons
-    #doublons = tableauFinal[tableauFinal.duplicated()]
+    # convertir en numérique
+    doublons["Longitude"] = pd.to_numeric(tableauFinal["Longitude"])
+    doublons["Latitude"] = pd.to_numeric(tableauFinal["Latitude"])
+    
+    # supprimer doublons géographiques
+    doublons= doublons.drop_duplicates(subset=["Longitude", "Latitude"],keep="first")
 
     return doublons
 
 def filtrer_intersections(tableauFinal):
     # Filtrer les intersections en fonction du type de voie
     typevoie = {"Avenue","Esplanade", "Boulevard"}  # Ajouter d'autres types de voies si nécessaire
-    tableauFinal= tableauFinal[tableauFinal["Intersections"].str.contains("|".join(typevoie), case=False)]
+    df= tableauFinal[tableauFinal["Intersections"].str.contains(typevoie.item(), case=False)]
 
-
-    return intersections_commune
+    return df
 
 
 #demander le nom de la commune
@@ -79,7 +229,7 @@ ville=input("Entrez le nom de la commune : ")
 #demander le nom du fichier csv
 nom = input("Entrez le nom du fichier CSV (sans l'extension .csv) : ")
 path="data/output/" + nom + ".csv"
-
-
+tableau=charger_intersections(path, ville)
+print (tableau)
 
 
